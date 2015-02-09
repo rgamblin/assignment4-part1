d16a5566313d64f9a6b55b07e1fdc5a14d9f534d
Rodger Gamblin <gamblinr@onid.oregonstate.edu>
2015-02-08 21:16:57 -0800

    Fixed multtable's recognition of non-integer output

--- src/multtable.php
+++ src/multtable.php
@@ -15,45 +15,61 @@
     
     if (!isset($_GET['min-multiplicand'])) {
         echo 'Missing parameter min-multiplicand<br>';
-    } else if (!is_integer($_GET['min-multiplicand'] + 0)) {
-        echo 'min-multiplicand must be an integer<br>';
     } else
         $checks++;
    
     if (!isset($_GET['max-multiplicand'])) {
         echo 'Missing parameter max-multiplicand<br>';
-    } else if (!is_integer($_GET['max-multiplicand'] + 0)) {
-        echo 'max-multiplicand must be an integer<br>';
     } else
         $checks++;
     
     if (!isset($_GET['min-multiplier'])) {
         echo 'Missing parameter min-multiplier<br>';
-    } else if (!is_integer($_GET['min-multiplier'] + 0)) {
-        echo 'min-multiplier must be an integer<br>';
     } else
         $checks++;
     
     if (!isset($_GET['max-multiplier'])) {
         echo 'Missing parameter max-multiplier<br>';
-    } else if (!is_integer($_GET['max-multiplier'] + 0)) {
-        echo 'max-multiplier must be an integer<br>';
     } else
         $checks++;
         
-    if (isset($_GET['min-multiplicand'], $_GET['max-multiplicand']) && is_integer($_GET['min-multiplicand'] + 0) && is_integer($_GET['max-multiplicand'] + 0) && $_GET['min-multiplicand'] > $_GET['max-multiplicand']) {
+    if (isset($_GET['min-multiplicand'], $_GET['max-multiplicand']) && is_integer($_GET['min-multiplicand'] + 0) && is_integer($_GET['max-multiplicand'] + 0) && is_numeric($_GET['min-multiplicand']) && is_numeric($_GET['max-multiplicand']) && $_GET['min-multiplicand'] > $_GET['max-multiplicand']) {
       echo 'Minimun multiplicand larger than maximum<br>';
     }
     else
       $checks++;
       
-    if (isset($_GET['min-multiplier'], $_GET['max-multiplier']) && is_integer($_GET['min-multiplier'] + 0) && is_integer($_GET['max-multiplier'] + 0) && $_GET['min-multiplier'] > $_GET['max-multiplier']) {
-      echo 'Minimun multiplier larger than maximum<br>';
+    if (isset($_GET['min-multiplier'], $_GET['max-multiplier']) && is_integer($_GET['min-multiplier'] + 0) && is_integer($_GET['max-multiplier'] + 0) && is_numeric($_GET['min-multiplier']) && is_numeric($_GET['max-multiplier']) && $_GET['min-multiplier'] > $_GET['max-multiplier']) {
+      echo 'Minimum multiplier larger than maximum<br>';
     }
     else
       $checks++;
     
-    if ($checks == 6)
+    if (isset($_GET['min-multiplier']) && (!is_integer($_GET['min-multiplier'] + 0) || !is_numeric($_GET['min-multiplier']))) 
+      echo 'Minimum multiplier must be an integer<br>';
+    else 
+      $checks++;
+    
+    if (isset($_GET['max-multiplier']) && (!is_integer($_GET['max-multiplier'] + 0) || !is_numeric($_GET['max-multiplier']))) 
+      echo 'Maximum multiplier must be an integer<br>';
+    else 
+      $checks++;
+    
+    if (isset($_GET['min-multiplicand']) && (!is_integer($_GET['min-multiplicand'] + 0) || !is_numeric($_GET['min-multiplicand']))) 
+      echo 'Minimum multiplicand must be an integer<br>';
+    else 
+      $checks++;
+    
+    if (isset($_GET['max-multiplicand']) && (!is_integer($_GET['max-multiplicand'] + 0) || !is_numeric($_GET['max-multiplicand']))) 
+      echo 'Maximum multiplicand must be an integer<br>';
+    else 
+      $checks++;
+    
+    
+    
+    
+    
+    if ($checks == 10)
       {
         echo '<table border=1> <thead> <tr> <th>';
         
